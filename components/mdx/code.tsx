483a62c38bc15ab33ced8c211143e33cd7232a61
James <james@eli.cx>
2024-09-27 22:20:26 +0100

    implement shiki for code

---
+++ components/mdx/code.tsx
@@ -0,0 +1,28 @@
+import { codeToHtml } from 'shiki';
+
+import { CodeBasic } from './code-basic';
+
+type Props = React.HTMLAttributes<HTMLElement> & {
+  shouldNotBeInline?: boolean;
+};
+
+export const Code = async ({ children, className, shouldNotBeInline }: Props) => {
+  const language = className?.match(/language-(\w+)/)?.[1];
+
+  if (!language || typeof children !== 'string') {
+    return <CodeBasic isInline={!shouldNotBeInline && !language}>{children}</CodeBasic>;
+  }
+
+  const transformed = await codeToHtml(children, {
+    lang: language,
+    themes: {
+      light: 'github-light',
+      dark: 'github-dark',
+    },
+  });
+
+  return (
+    // eslint-disable-next-line react/no-danger
+    <CodeBasic dangerouslySetInnerHTML={{ __html: transformed }} />
+  );
+};
--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       BASE_URL: string;
       D1_ANALYTICS?: D1Database;
+      NEXT_DEPLOYMENT_ID?: string;
     }
   }
 }
